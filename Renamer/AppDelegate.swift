ae68e6232712fc9533dc3794719f8a80851c8307
Frizlab <fload@me.com>
2022-02-23 18:57:40 +0100

    Initial Commit

---
+++ Renamer/AppDelegate.swift
@@ -0,0 +1,25 @@
+/*
+ * AppDelegate.swift
+ * Renamer
+ *
+ * Created by François Lamboley on 2022/02/23.
+ */
+
+import Cocoa
+
+
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+	
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+	}
+	
+	func applicationWillTerminate(_ aNotification: Notification) {
+	}
+	
+	func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+		return true
+	}
+	
+}
