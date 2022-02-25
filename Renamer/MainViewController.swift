81c079ce8480c288d6114a8487570815c830afbc
Frizlab <fload@me.com>
2022-02-25 18:04:52 +0100

    Bind the tables views to a view model

--- Renamer/MainViewController.swift
+++ Renamer/MainViewController.swift
@@ -11,8 +11,20 @@ import Cocoa
 
 class MainViewController : NSViewController {
 	
+	@IBOutlet var tableViewFiles: NSTableView!
+	@IBOutlet var tableViewFilenames: NSTableView!
+	
+	@IBOutlet var arrayControllerFiles: NSArrayController!
+	@IBOutlet var arrayControllerFilenames: NSArrayController!
+	
+	@objc dynamic var files = [URL]()
+	@objc dynamic var filenames = [String]()
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		
+		files = [URL(fileURLWithPath: "/amazing/path")]
+		filenames = ["Amazing New Name"]
 	}
 	
 	override var representedObject: Any? {
