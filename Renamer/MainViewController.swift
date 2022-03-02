1fddc1c002a6546a8dfdfadb53a09b090a925910
Frizlab <fload@me.com>
2022-03-02 11:12:56 +0100

    Implement drag and drop for filenames table view

--- Renamer/MainViewController.swift
+++ Renamer/MainViewController.swift
@@ -29,6 +29,7 @@ class MainViewController : NSViewController, NSTableViewDataSource, NSUserInterf
 		super.viewDidLoad()
 		
 		tableViewFiles.registerForDraggedTypes([.fileURL, DraggedFile.draggedType])
+		tableViewFilenames.registerForDraggedTypes([.fileURL, DraggedFilename.draggedType])
 		
 		files = []
 		filenames = []
@@ -122,8 +123,103 @@ class MainViewController : NSViewController, NSTableViewDataSource, NSUserInterf
 	}
 	
 	func tableView(_ tableView: NSTableView, pasteboardWriterForRow row: Int) -> NSPasteboardWriting? {
-		guard tableView == tableViewFiles else {return nil}
-		return DraggedFile(sourceRow: row)
+		switch tableView {
+			case tableViewFiles:     return DraggedFile(sourceRow: row)
+			case tableViewFilenames: return DraggedFilename(sourceRow: row)
+			default:                 return nil
+		}
+	}
+	
+	func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableView.DropOperation) -> NSDragOperation {
+		switch tableView {
+			case tableViewFiles:
+				let canRead = (
+					info.draggingPasteboard.canReadObject(forClasses: [NSURL.self], options: readOptions) ||
+					info.draggingPasteboard.canReadObject(forClasses: [DraggedFile.self], options: nil)
+				)
+				return (canRead && dropOperation == .above) ? .generic : .init()
+				
+			case tableViewFilenames:
+				let canRead = info.draggingPasteboard.canReadObject(forClasses: [DraggedFilename.self], options: nil)
+				return (canRead && dropOperation == .above) ? .generic : .init()
+				
+			default:
+				return .init()
+		}
+		
+	}
+	
+	func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableView.DropOperation) -> Bool {
+		switch tableView {
+			case tableViewFiles:
+				if let draggedFiles = info.draggingPasteboard.readObjects(forClasses: [DraggedFile.self], options: nil) as? [DraggedFile], !draggedFiles.isEmpty {
+					let draggedRows = Set(draggedFiles.map{ $0.sourceRow }).sorted()
+					
+					let delta = draggedRows.firstIndex{ $0 >= row } ?? draggedRows.count
+					let added = draggedRows.map{ (arrayControllerFiles.arrangedObjects as! [URL])[$0] }
+					
+					arrayControllerFiles.remove(atArrangedObjectIndexes: IndexSet(draggedRows))
+					arrayControllerFiles.insert(contentsOf: added, atArrangedObjectIndexes: IndexSet(draggedRows.enumerated().map{ row - delta + $0.offset }))
+					
+					return true
+				}
+				if let urls = info.draggingPasteboard.readObjects(forClasses: [NSURL.self], options: readOptions) as? [URL], !urls.isEmpty {
+					urls.reversed().forEach{ arrayControllerFiles.insert($0, atArrangedObjectIndex: row) }
+					return true
+				}
+				return false
+				
+			case tableViewFilenames:
+				if let draggedFilenames = info.draggingPasteboard.readObjects(forClasses: [DraggedFilename.self], options: nil) as? [DraggedFilename], !draggedFilenames.isEmpty {
+					let draggedRows = Set(draggedFilenames.map{ $0.sourceRow }).sorted()
+					
+					let delta = draggedRows.firstIndex{ $0 >= row } ?? draggedRows.count
+					let added = draggedRows.map{ (arrayControllerFilenames.arrangedObjects as! [String])[$0] }
+					
+					arrayControllerFilenames.remove(atArrangedObjectIndexes: IndexSet(draggedRows))
+					arrayControllerFilenames.insert(contentsOf: added, atArrangedObjectIndexes: IndexSet(draggedRows.enumerated().map{ row - delta + $0.offset }))
+					
+					return true
+				}
+				return false
+				
+			default:
+				return false
+		}
+		
+	}
+	
+	private let readOptions = [NSPasteboard.ReadingOptionKey.urlReadingFileURLsOnly: false]
+	
+	private func updateStateVars() {
+		guard files.count == filenames.count else {
+			errorMessage = NSLocalizedString("cannot rename err: files and filenames do not have the same number of elements", value: "⚠️ The files and file names tables do not have the same number of elements.", comment: "Text shown when the number of elements in the files and the file names tables are not equal.")
+			canRename = false
+			return
+		}
+		guard !files.isEmpty else {
+			errorMessage = nil
+			canRename = false
+			return
+		}
+		guard Set(files).count == files.count else {
+			errorMessage = NSLocalizedString("cannot rename err: files has duplicates", value: "⚠️ The files table contains the same item more than once.", comment: "Text shown when the files table contains the same item more than once.")
+			canRename = false
+			return
+		}
+		guard Set(filenames).count == filenames.count else {
+			errorMessage = NSLocalizedString("cannot rename err: filenames has duplicates", value: "⚠️ The file names table contains the same item more than once.", comment: "Text shown when the filenames table contains the same item more than once.")
+			canRename = false
+			return
+		}
+		guard !filenames.contains(where: { $0.contains("/") }) else {
+			errorMessage = NSLocalizedString("cannot rename err: filenames contains invalid name", value: "⚠️ The file names table contains an item with an invalid name (check for punctuation, etc.)", comment: "Text shown when the filenames table contains an item with an invalid name.")
+			canRename = false
+			return
+		}
+		
+		errorMessage = nil
+		canRename = true
 	}
 	
 	private final class DraggedFile : NSObject, NSPasteboardWriting, NSPasteboardReading {
@@ -163,68 +259,41 @@ class MainViewController : NSViewController, NSTableViewDataSource, NSUserInterf
 		
 	}
 	
-	func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableView.DropOperation) -> NSDragOperation {
-		guard tableView == tableViewFiles else {return .init()}
+	private final class DraggedFilename : NSObject, NSPasteboardWriting, NSPasteboardReading {
 		
-		let canRead = (
-			info.draggingPasteboard.canReadObject(forClasses: [NSURL.self], options: readOptions) ||
-			info.draggingPasteboard.canReadObject(forClasses: [DraggedFile.self], options: nil)
-		)
-		return (canRead && dropOperation == .above) ? .generic : .init()
-	}
-	
-	func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableView.DropOperation) -> Bool {
-		guard tableView == tableViewFiles else {return false}
+		static var draggedType = NSPasteboard.PasteboardType("com.happn.renamer.main-view-controller.filenames")
 		
-		if let draggedFiles = info.draggingPasteboard.readObjects(forClasses: [DraggedFile.self], options: nil) as? [DraggedFile], !draggedFiles.isEmpty {
-			let draggedRows = Set(draggedFiles.map{ $0.sourceRow }).sorted()
-			
-			let delta = draggedRows.firstIndex{ $0 >= row } ?? draggedRows.count
-			let added = draggedRows.map{ (arrayControllerFiles.arrangedObjects as! [URL])[$0] }
-			
-			arrayControllerFiles.remove(atArrangedObjectIndexes: IndexSet(draggedRows))
-			arrayControllerFiles.insert(contentsOf: added, atArrangedObjectIndexes: IndexSet(draggedRows.enumerated().map{ row - delta + $0.offset }))
-			
-			return true
-		}
-		if let urls = info.draggingPasteboard.readObjects(forClasses: [NSURL.self], options: readOptions) as? [URL], !urls.isEmpty {
-			urls.reversed().forEach{ arrayControllerFiles.insert($0, atArrangedObjectIndex: row) }
-			return true
-		}
-		return false
-	}
-	
-	private let readOptions = [NSPasteboard.ReadingOptionKey.urlReadingFileURLsOnly: false]
-	
-	private func updateStateVars() {
-		guard files.count == filenames.count else {
-			errorMessage = NSLocalizedString("cannot rename err: files and filenames do not have the same number of elements", value: "⚠️ The files and file names tables do not have the same number of elements.", comment: "Text shown when the number of elements in the files and the file names tables are not equal.")
-			canRename = false
-			return
-		}
-		guard !files.isEmpty else {
-			errorMessage = nil
-			canRename = false
-			return
-		}
-		guard Set(files).count == files.count else {
-			errorMessage = NSLocalizedString("cannot rename err: files has duplicates", value: "⚠️ The files table contains the same item more than once.", comment: "Text shown when the files table contains the same item more than once.")
-			canRename = false
-			return
-		}
-		guard Set(filenames).count == filenames.count else {
-			errorMessage = NSLocalizedString("cannot rename err: filenames has duplicates", value: "⚠️ The file names table contains the same item more than once.", comment: "Text shown when the filenames table contains the same item more than once.")
-			canRename = false
-			return
-		}
-		guard !filenames.contains(where: { $0.contains("/") }) else {
-			errorMessage = NSLocalizedString("cannot rename err: filenames contains invalid name", value: "⚠️ The file names table contains an item with an invalid name (check for punctuation, etc.)", comment: "Text shown when the filenames table contains an item with an invalid name.")
-			canRename = false
-			return
+		static func readableTypes(for pasteboard: NSPasteboard) -> [NSPasteboard.PasteboardType] {
+			[draggedType]
 		}
 		
-		errorMessage = nil
-		canRename = true
+		var sourceRow: Int
+		
+		init(sourceRow: Int) {
+			self.sourceRow = sourceRow
+		}
+		
+		init?(pasteboardPropertyList propertyList: Any, ofType type: NSPasteboard.PasteboardType) {
+			guard type == Self.draggedType, let r = propertyList as? Int else {
+				return nil
+			}
+			self.sourceRow = r
+		}
+		
+		func writableTypes(for pasteboard: NSPasteboard) -> [NSPasteboard.PasteboardType] {
+			return [Self.draggedType]
+		}
+		
+		func pasteboardPropertyList(forType type: NSPasteboard.PasteboardType) -> Any? {
+			assert(type == Self.draggedType)
+			return sourceRow
+		}
+		
+		static func readingOptions(forType type: NSPasteboard.PasteboardType, pasteboard: NSPasteboard) -> NSPasteboard.ReadingOptions {
+			assert(type == Self.draggedType)
+			return [.asPropertyList]
+		}
+		
 	}
 	
 }
